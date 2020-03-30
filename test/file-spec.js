be225eba8c44ada47d706502cb51ec364b41a773
Trevor Pellegrini <trevorpellegrini@gmail.com>
2020-03-30 12:02:03 -0700

    files

---
+++ test/file-spec.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var chai = require('chai')
+var templatize = require('../index')
+var isNode
+
+if (process !== undefined) {
+    isNode = true
+}
+
+describe.only('templatize File', function () {
+    it('first', function () {
+        var file = __dirname + '/test-data/test-file1.txt'
+        var source = {
+            name: 'bob',
+            addresses: ['somewhere']
+        }
+        var compare = fs.readFileSync(__dirname + '/test-data/test-file2.txt', 'utf8')
+        var result = templatize.file(file, source)
+        chai.expect(result).eq(compare)
+    })
+})
